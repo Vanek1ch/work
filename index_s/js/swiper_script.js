6c58041baba75e567e02f0b51474c665c7eeae23
iluha481 <paionnonionn@gmail.com>
2024-09-20 01:50:51 +0300

    swiper pagination

--- index_s/js/swiper_script.js
+++ index_s/js/swiper_script.js
@@ -1,16 +1,11 @@
 // Инициализация Swiper
 var swiper = new Swiper('.swiper-container', {
-    // Параметры
     loop: true,
     pagination: {
         el: '.swiper-pagination',
-        clickable: false,
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        
     },
     slidesPerView: 1, 
-    spaceBetween: 0, 
+
     touchEventsTarget: 'container',
 });
